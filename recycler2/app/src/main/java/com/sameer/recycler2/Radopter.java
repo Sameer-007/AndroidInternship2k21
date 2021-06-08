56d5cf1b7db6594b1bce9b92893c6385ae928ce1
Sameer-007 <sksameer429@gmail.com>
2021-06-08 17:09:10 +0530

    recycler 2

--- recycler2/app/src/main/java/com/sameer/recycler2/Radopter.java
+++ recycler2/app/src/main/java/com/sameer/recycler2/Radopter.java
@@ -1,8 +1,11 @@
 package com.sameer.recycler2;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -25,23 +28,33 @@ public class Radopter extends RecyclerView.Adapter<Radopter.MyviewHolder> {
     @org.jetbrains.annotations.NotNull
     @Override
     public MyviewHolder onCreateViewHolder(@NonNull @org.jetbrains.annotations.NotNull ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(ct).inflate(R.layout.row,parent,false);
+        MyviewHolder myviewHolder  = new MyviewHolder(v);
+        return myviewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull @org.jetbrains.annotations.NotNull Radopter.MyviewHolder holder, int position) {
-
+    holder.iv.setImageResource(myimages[position]);
+    holder.tv1.setText(mynames[position]);
+    holder.tv2.setText(mynames[position]);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+
+        return myimages.length;
     }
 
     public class MyviewHolder extends RecyclerView.ViewHolder {
+        ImageView iv;
+        TextView tv1,tv2;
+
         public MyviewHolder(@NonNull @org.jetbrains.annotations.NotNull View itemView) {
             super(itemView);
-
+            iv = itemView.findViewById(R.id.image);
+            tv1 = itemView.findViewById(R.id.name);
+            tv2 = itemView.findViewById(R.id.price);
         }
     }
 }
